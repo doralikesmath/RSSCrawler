3c07e653536978160af28a5dc3c6264edd4675ea
doralikesmath <bachmap2003@gmail.com>
2021-05-06 17:00:46 +0700

    Heroku

---
+++ app.py
@@ -0,0 +1,36 @@
+"""
+                                ╔═════════════════════════════════════════════╗
+                                ║             .-') _                          ║
+                                ║            (  OO) )                         ║
+                                ║          ,(_)----. .---. .-----.            ║
+                                ║          |       |/_   |/  -.   \           ║
+                                ║          '--.   /  |   |'-' _'  |           ║
+                                ║          (_/   /   |   |   |_  <            ║
+                                ║           /   /___ |   |.-.  |  |           ║
+                                ║          |        ||   |\ `-'   /           ║
+                                ║          `--------'`---' `----''     ©2021  ║
+                                ╚═════════════════════════════════════════════╝
+"""
+from flask import Flask
+from flask import send_file, jsonify
+app = Flask(__name__)
+
+
+@app.route("/download")
+def download():
+    path = "./corpus.txt"
+    return send_file(path, as_attachment=True)
+
+
+@app.route("/healthcheck")
+def healthcheck():
+    return jsonify("Ok")
+
+
+@app.route("/")
+def default():
+    return jsonify("There is nothing here")
+
+
+if __name__ == "__main__":
+    app.run(port=5000,debug=True)
